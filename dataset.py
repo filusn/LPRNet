4d35b7122edcffe51a20aaa1dded5ff75e3f011a
Filip Noworolnik <fnoworolnik@gmail.com>
2022-09-08 17:33:25 +0200

    Included blank chars in label

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import pathlib
+from re import L
 
 from PIL import Image
 import torch
@@ -25,6 +26,18 @@ class LicensePlateDataset(Dataset):
         img = Normalize(mean, std)(img)
 
         label = self.imgs[index].stem.split('_')[0]
+        label = label.upper()
+        label = ''.join(ch for ch in label if ch.isalnum())
+
+        while len(label) < 9:
+            splitted = False
+            for i in range(len(label) - 1):
+                if label[i] == label[i + 1]:
+                    label = label[: i + 1] + '-' + label[i + 1 :]
+                    splitted = True
+                    break
+            if not splitted:
+                label += '-'
         label = [config.CHARS_DICT[char] for char in label]
         label = torch.tensor(label)
 
@@ -39,3 +52,8 @@ if __name__ == '__main__':
     print(img.std())
     print(label)
     print(length)
+    print(len(lpd))
+    from torch.utils.data import DataLoader
+
+    dl = DataLoader(lpd)
+    print(len(dl))
