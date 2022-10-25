da8b51b7e5c6c8ed082f9f47631098761e6d0b04
Filip Noworolnik <fnoworolnik@gmail.com>
2022-10-25 11:38:03 +0200

    Update config with paths to datasets

--- config.py
+++ config.py
@@ -2,15 +2,21 @@ import os
 import pathlib
 import random
 
+import numpy as np
 import torch
 from torchvision import transforms
 
+TRAIN_DATASET = ''
+VAL_DATASET = ''
+TEST_DATASET = ''
+
+DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 SEED = 10
-DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-EPOCHS = 100
+EPOCHS = 300
 BATCH_SIZE = 32
 LEARNING_RATE = 1e-3
+
 MODELS_PATH = pathlib.Path('saved_models')
 if not MODELS_PATH.exists():
     MODELS_PATH.mkdir()
@@ -28,11 +34,11 @@ CHARS_DICT = {char: i for i, char in enumerate(CHARS)}
 BLANK_SIGN = '-'
 INPUT_LEN = 30
 
-# TODO: Calculate mean and std of all images
+# TODO: Include normalization.
+# TODO: Include augmentations for training on real dataset.
 transforms_train = transforms.Compose(
     [
         transforms.ToTensor(),
-        # transforms.Normalize()
     ]
 )
 
@@ -40,7 +46,7 @@ transforms_train = transforms.Compose(
 def seed_everything(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
-    # np.random.seed(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
