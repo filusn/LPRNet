b7cf2230725c331293b1d0fdfafd34884c9b6d68
Filip Noworolnik <fnoworolnik@gmail.com>
2022-10-25 11:41:05 +0200

    Update dataset module with clearings, todos and comments

--- dataset.py
+++ dataset.py
@@ -1,8 +1,7 @@
 import pathlib
-from re import L
 
-from PIL import Image
 import torch
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.transforms import Normalize, ToTensor
 
@@ -21,55 +20,24 @@ class LicensePlateDataset(Dataset):
     def __getitem__(self, index):
         img = Image.open(self.imgs[index])
         img = img.resize((128, 28))
-        # img = img.resize((94, 24))
+
+        # TODO: Move to the augmentations for train/test ds.
         img = ToTensor()(img)
         mean, std = img.mean([1, 2]), img.std([1, 2])
         img = Normalize(mean, std)(img)
 
         label = self.imgs[index].stem.split('_')[0]
         label = label.upper()
+
+        # Checking for alphanumeric characters.
+        # May be removed with a proper dataset.
         label = ''.join(ch for ch in label if ch.isalnum())
         length = len(label)
-        # TODO: Move to the collate_fn with torch padding
-        # for i in range(len(label) - 1):
-        #     if label[i] == label[i + 1]:
-        #         label = label[: i + 1] + '-' + label[i + 1 :]
 
+        # TODO: Move to the collate_fn with torch padding
         while len(label) < config.INPUT_LEN:
             label += '-'
         label = [config.CHARS_DICT[char] for char in label]
         label = torch.tensor(label)
 
         return img, label, length
-
-
-# def collate_fn(batch):
-#     imgs = []
-#     labels = []
-#     lengths = []
-#     for _, sample in enumerate(batch):
-#         img, label, length = sample
-#         imgs.append(torch.from_numpy(img))
-#         labels.extend(label)
-#         lengths.append(length)
-#     labels = np.asarray(labels).flatten().astype(np.int)
-
-#     return (torch.stack(imgs, 0), torch.from_numpy(labels), lengths)
-
-
-if __name__ == '__main__':
-    lpd = LicensePlateDataset('data2')
-    img, label, length = next(iter(lpd))
-    print(img.size())
-    print(img.mean())
-    print(img.std())
-    print(label)
-    print(length)
-    print(len(lpd))
-    # from torch.utils.data import DataLoader
-
-    # dl = DataLoader(lpd, batch_size=2)
-    # print(len(dl))
-    # for sample in dl:
-    #     print(sample)
-    #     break
